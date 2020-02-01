eadc44ccda1c14e8c6072817263260cad684f8ea
Temitope Joan Akangbe <49322592+TemitopeAkangbe@users.noreply.github.com>
2020-02-01 18:20:11 +0100

    Updated

---
+++ scripts.js
@@ -0,0 +1,17 @@
+console.log("form");
+const form = document.querySelector("form");
+form.addEventListener('submit', validateForm);
+
+function validateForm () {
+    const password = document.getElementById("password").value
+    const confirmpassword = document.getElementById("confirmpassword").value
+
+     if (password != confirmpassword) {
+         alert("Passwords do not match.");   
+     }
+
+     else {
+      alert ("You are registered");
+     }
+    }
+
